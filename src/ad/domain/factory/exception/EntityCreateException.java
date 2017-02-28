8c344c409356ba984e0f8a6a6561ed074479eb74
Sergey Yermochenko <yermochenko@gmail.com>
2017-02-28 15:52:02 +0300

    Стиль кода убрана аннотация SupressWarnings

--- src/ad/domain/factory/exception/EntityCreateException.java
+++ src/ad/domain/factory/exception/EntityCreateException.java
@@ -1,6 +1,5 @@
 package ad.domain.factory.exception;
 
-@SuppressWarnings("serial")
 public class EntityCreateException extends Exception {
     public EntityCreateException(Throwable cause) {
         super(cause);
